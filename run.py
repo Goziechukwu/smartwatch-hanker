041e00de16ec8fb7eca6e4f7024cfe9218bfbb91
Goziechukwu <>
2023-10-13 15:12:47 +0000

    Create function that gets customer orders

--- run.py
+++ run.py
@@ -18,15 +18,14 @@ SHEET = GSPREAD_CLIENT.open('smartwatch_hanker')
 # Get available stock data
 stock = SHEET.worksheet('stock')
 stock_data = stock.get_all_records()
-print(stock_data)
 
-# # Get customer orders from the inventory manager
-# def get_customer_orders():
-#     orders = []
-#     while True:
-#         product_name = input("Enter product name (or 'done' to finish): ")
-#         if product_name.lower() == 'done':
-#             break
-#         quantity = int(input(f"Enter quantity of {product_name}: "))
-#         orders.append({"Product": product_name, "Quantity": quantity})
-#     return orders
+# Get customer orders from the inventory manager
+def get_customer_orders():
+    orders = []
+    while True:
+        product = input("Enter product name (or 'done' to finish): ")
+        if product.lower() == 'done':
+            break
+        quantity = int(input(f"Enter quantity of {product}: "))
+        orders.append({"Product": product, "Quantity": quantity})
+    return orders
