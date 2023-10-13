eb75482b1347419fbac3d68e4e2860366ee8abd1
Goziechukwu <>
2023-10-13 16:57:56 +0000

    Add comments in the customer order function

--- run.py
+++ run.py
@@ -19,10 +19,26 @@ SHEET = GSPREAD_CLIENT.open('smartwatch_hanker')
 stock = SHEET.worksheet('stock')
 stock_data = stock.get_all_records()
 
+
 # Get customer orders from the inventory manager
 def get_customer_orders():
+    """
+    Get customer orders input from the user.
+    I run a while loop to collect a valid string of product name
+    and of product quantity from the user via the terminal,
+    The while loop will repeatedly request data, until it is valid
+    and the user types the word- 'done'.
+    """    
+    
     orders = []
+
     while True:
+        print("Please enter product name where asked,")
+        print("and then enter quantity ordered where asked too.")
+        print("Type the word 'done' without the quotations when you finish.")
+        print("Product name should be correctly typed in lowercase characters")
+        print("and quantity should be numbers only\n")
+
         product = input("Enter product name (or 'done' to finish): ")
         if product.lower() == 'done':
             break
@@ -30,8 +46,10 @@ def get_customer_orders():
         orders.append({"Product": product, "Quantity": quantity})
     return orders
 
+
 # Compare customer orders with available stock
 def check_inventory(orders):
+    
     for order in orders:
         product = order['Product']
         ordered_quantity = order['Quantity']
@@ -48,6 +66,7 @@ def check_inventory(orders):
             print(f"We can fulfill the request for {ordered_quantity} units of {product}.")
         else:
             print(f"Insufficient stock for {product}. Available quantity: {available_quantity}. Ordered quantity: {ordered_quantity}.")
+
 
 # Get customer orders from the inventory manager
 customer_orders = get_customer_orders()
